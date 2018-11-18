5c2bb06aab14a11efc3bb8e2d84bfba2551929e2
Matt Outen <mattoutenphysed@gmail.com>
2018-11-18 13:49:12 -0600

    view fb config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: 'AIzaSyDPAkMHw0FXy6Ix4-OrqtkgPIjWb2qS-Vc',
+  authDomain: 'baby-app-199fa.firebaseapp.com',
+  databaseURL: 'https://baby-app-199fa.firebaseio.com',
+  projectId: 'baby-app-199fa',
+  storageBucket: 'baby-app-199fa.appspot.com',
+  messagingSenderId: '643236783327'
+};
+
+export default config;
