e62274c0a9936650cd1b6c532cb175a7114bd757
Matt Outen <mattoutenphysed@gmail.com>
2018-11-18 14:05:12 -0600

    fixed landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-import { Container } from 'semantic-ui-react';
+import { Container, Image } from 'semantic-ui-react';
 
 const Landing = () => (
   <Container>
-    <h2>Landing</h2>
-    <p>Welcome the baby app</p>
+    <Container text centered>
+      <h2> Baby Tracker!</h2>
+      <p>Keeping track of baby Monroe</p>
+    </Container>
+    <Image
+      style={{ marginTop: '10px' }}
+      src="https://i.imgur.com/aUEr4kn.png"
+      size="large"
+      bordered
+      centered
+      circular
+    />
   </Container>
 );
 
